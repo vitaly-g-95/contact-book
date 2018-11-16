6213881f9d5afd822d1fe9ed861334ac742d5709
Vitaly Gumenyak <vetalgr@gmail.com>
2018-11-16 07:37:56 +0200

    modified App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import AddContactButton from './containers/AddContactButton';
+import Modal from './containers/Modal';
 
 class App extends Component {
   render() {
     return (
-      <div className="container my-5">
-        
-      </div>
+      <Fragment>
+        <div className="container my-5">
+          <AddContactButton
+            className="d-table mx-auto"
+            text="Add Contact"
+          />
+        </div>
+
+        <Modal />
+      </Fragment>
     );
   }
 }
