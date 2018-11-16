aea7ed3a3099dba34de0cf05e565a773ad3d44bc
Vitaly Gumenyak <vetalgr@gmail.com>
2018-11-16 07:35:46 +0200

    modified ui reducer

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -1,5 +1,30 @@
-const initialState = {};
+const initialState = {
+  modalContent: null,
+  modalStyle: {}
+};
 
 export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'MODAL_OPEN':
+      const { top, left, height, width } = action.target.getBoundingClientRect();
+
+      return Object.assign({}, state, {
+        modalContent: action.content,
+        modalStyle: {
+          top: top + height / 2,
+          left: left + width / 2
+        }
+      });
+    case 'MODAL_CLOSE':
+      if (action.target !== action.currentTarget) {
+        break;
+      }
+
+      return Object.assign({}, state, {
+        modalContent: null
+      });
+    default:
+  }
+
   return state;
 };
