eb6a720d6342eac131317907c719c83b1ddda08d
Vitaly Gumenyak <vetalgr@gmail.com>
2018-11-20 06:03:52 +0200

    modified AddContactButton container

--- src/containers/AddContactButton.js
+++ src/containers/AddContactButton.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import Button from '../components/Button';
+import AddContactForm from './AddContactForm';
 
 const mapStateToProps = store => ({});
 
 const mapDispatchToProps = dispatch => ({
   onClick: ({ target }) => {
-    dispatch({ type: 'MODAL_OPEN', content: 'test', target });
+    dispatch({ type: 'OPEN_MODAL', content: <AddContactForm />, target });
   }
 });
 
